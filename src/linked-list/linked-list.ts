ef515fbfbb5890107dc8d36ab1cbd27fa3d0cc33
GabrielFrahm <gabriel_frahm@hotmail.com>
2024-08-22 08:07:13 -0300

    feat(): implements pop unshift shift methods

--- src/linked-list/linked-list.ts
+++ src/linked-list/linked-list.ts
@@ -1,8 +1,8 @@
 import { Node } from "./node";
 
 export class LinkedList<T> {
-    head: Node<T>;
-    tail: Node<T>;
+    head: Node<T> | null;
+    tail: Node<T> | null;
     length: number;
 
     constructor(value: T){
@@ -38,10 +38,53 @@ export class LinkedList<T> {
         this.head = newNode;
         this.tail = newNode;
       }else{
-        this.tail.next = newNode;
+        this.tail!.next = newNode;
         this.tail = newNode;
       }
       this.length++
       return this;
+    }
+
+    pop(){
+      if(!this.head) return undefined;
+      let temp = this.head;
+      let pre = this.head;
+      while(temp.next){
+        pre = temp;
+        temp = temp.next
+      }
+      this.tail = pre;
+      this.tail.next = null;
+      this.length--;
+      if(this.length === 0){
+        this.head = null;
+        this.tail = null;
+      }
+      return temp;
+    }
+
+    unshift(value: T){
+      const newNode = new Node<T>(value);
+      if(!this.head){
+        this.head = newNode;
+        this.tail = newNode;
+      }else{
+        newNode.next = this.head;
+        this.head = newNode;
+      }
+      this.length++;
+      return this;
+    }
+
+    shift(){
+      if(!this.head) return undefined;
+      let temp = this.head;
+      this.head = this.head.next;
+      temp.next = null;
+      this.length--;
+      if(this.length === 0){
+        this.tail = null;
+      }
+      return temp;
     }
 }
--- test/linked-list.spec.ts
+++ test/linked-list.spec.ts
@@ -34,4 +34,123 @@ describe("Deve testa uma lista encadeada", () => {
     })
     expect(ll.getLength()).toBe(2)
   })
+
+  test("Deve adicionar um item no final da lista quando ela estiver vazia", () => {
+    const ll = new LinkedList<number>(4);
+    ll.pop();
+    expect(ll.getLength()).toBe(0)
+    ll.push(5)
+    expect(ll.getHead()).toMatchObject({
+      value: 5,
+      next: null
+    })
+    expect(ll.getTail()).toMatchObject({
+      value: 5, next: null
+    })
+    expect(ll.getLength()).toBe(1)
+  })
+
+  test("Deve remover o ultimo item da lista POP", () => {
+    const ll = new LinkedList<{
+      name: string;
+      url: string;
+    }>({
+      name: "music-name",
+      url: "music-url"
+    });
+    ll.push({
+      name: "music-name-2",
+      url: "music-url-2"
+    });
+    expect(ll.getHead()).toMatchObject({
+      value: {
+        name: "music-name",
+        url: "music-url"
+      }, next: {
+          value:{
+            name: "music-name-2",
+            url: "music-url-2"
+          },
+          next: null
+      }
+    });
+    expect(ll.getLength()).toBe(2);
+    expect(ll.pop()).toMatchObject({
+      value:{
+        name: "music-name-2",
+        url: "music-url-2"
+      },
+      next: null
+    });
+    expect(ll.getLength()).toBe(1);
+    expect(ll.pop()).toMatchObject({
+      value:{
+        name: "music-name",
+        url: "music-url"
+      },
+      next: null
+    });
+    expect(ll.getLength()).toBe(0);
+    expect(ll.pop()).toBeUndefined();
+  })
+
+  test("deve adicionar um item no inicio da lista unshift", () => {
+    const ll = new LinkedList<{
+      name: string;
+      url: string;
+    }>({
+      name: "music-name",
+      url: "music-url"
+    });
+    expect(ll.getLength()).toBe(1);
+    ll.unshift({
+      name: "music-name-1",
+      url: "music-url-1"
+    });
+    expect(ll.getLength()).toBe(2);
+    expect(ll.getHead()).toMatchObject({
+      value: {
+        name: "music-name-1",
+        url: "music-url-1"
+      }, next: {
+          value:{
+            name: "music-name",
+            url: "music-url"
+          },
+          next: null
+      }
+    });
+  })
+
+  test("deve remover o primeiro item da lista shift", () => {
+    const ll = new LinkedList<{
+      name: string;
+      url: string;
+    }>({
+      name: "music-name",
+      url: "music-url"
+    });
+    ll.push({
+      name: "music-name-2",
+      url: "music-url-2"
+    });
+    expect(ll.getLength()).toBe(2)
+    expect(ll.shift()).toMatchObject({
+      value:{
+        name: "music-name",
+        url: "music-url"
+      },
+      next: null
+    });
+    expect(ll.getLength()).toBe(1)
+    expect(ll.shift()).toMatchObject({
+      value:{
+        name: "music-name-2",
+        url: "music-url-2"
+      },
+      next: null
+    });
+    expect(ll.getLength()).toBe(0)
+    expect(ll.shift()).toBeUndefined()
+  })
 })
